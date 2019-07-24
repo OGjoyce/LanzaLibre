5c214e1a77bb07b7ec74d5710a7411c04cd7ae96
Carlos <montiel@galileo.edu>
2019-07-24 15:37:43 -0500

    rev2
    fixed foreign keys

--- database/script.sql
+++ database/script.sql
@@ -21,6 +21,30 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Departamentos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Departamentos` (
+  `id` INT NOT NULL,
+  `Departamento` FLOAT NULL,
+  `longitud` FLOAT NULL,
+  `latitud` FLOAT NULL,
+  `altitud` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Servicios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Servicios` (
+  `idServicios` INT NOT NULL,
+  `nombre_servicio` VARCHAR(45) NOT NULL,
+  `estado` INT NOT NULL,
+  PRIMARY KEY (`idServicios`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Informacion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Informacion` (
@@ -31,7 +55,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Informacion` (
   `latitud` FLOAT NOT NULL,
   `altitud` FLOAT NOT NULL,
   `Direccion` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`idInformacion`))
+  `Departamentos_id` INT NOT NULL,
+  `Servicios_idServicios` INT NOT NULL,
+  PRIMARY KEY (`idInformacion`),
+  INDEX `fk_Informacion_Departamentos1_idx` (`Departamentos_id` ASC) ,
+  INDEX `fk_Informacion_Servicios1_idx` (`Servicios_idServicios` ASC) ,
+  CONSTRAINT `fk_Informacion_Departamentos1`
+    FOREIGN KEY (`Departamentos_id`)
+    REFERENCES `mydb`.`Departamentos` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Informacion_Servicios1`
+    FOREIGN KEY (`Servicios_idServicios`)
+    REFERENCES `mydb`.`Servicios` (`idServicios`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -56,44 +94,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Servicios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Servicios` (
-  `idServicios` INT NOT NULL,
-  `nombre_servicio` VARCHAR(45) NOT NULL,
-  `estado` INT NOT NULL,
-  `Informacion_idInformacion` INT NOT NULL,
-  PRIMARY KEY (`idServicios`),
-  INDEX `fk_Servicios_Informacion1_idx` (`Informacion_idInformacion` ASC),
-  CONSTRAINT `fk_Servicios_Informacion1`
-    FOREIGN KEY (`Informacion_idInformacion`)
-    REFERENCES `mydb`.`Informacion` (`idInformacion`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Departamentos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Departamentos` (
-  `id` INT NOT NULL,
-  `Informacion_idInformacion` INT NOT NULL,
-  `Departamento` FLOAT NULL,
-  `longitud` FLOAT NULL,
-  `latitud` FLOAT NULL,
-  `altitud` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Departamentos_Informacion1_idx` (`Informacion_idInformacion` ASC) ,
-  CONSTRAINT `fk_Departamentos_Informacion1`
-    FOREIGN KEY (`Informacion_idInformacion`)
-    REFERENCES `mydb`.`Informacion` (`idInformacion`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `mydb`.`ranking`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`ranking` (
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`ranking` (
   `rank` INT NULL,
   `Informacion_idInformacion` INT NOT NULL,
   PRIMARY KEY (`idrank`),
-  INDEX `fk_ranking_Informacion1_idx` (`Informacion_idInformacion` ASC),
+  INDEX `fk_ranking_Informacion1_idx` (`Informacion_idInformacion` ASC) ,
   CONSTRAINT `fk_ranking_Informacion1`
     FOREIGN KEY (`Informacion_idInformacion`)
     REFERENCES `mydb`.`Informacion` (`idInformacion`)
@@ -122,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Municipios` (
   `altitud` FLOAT NULL,
   `Departamentos_id` INT NOT NULL,
   PRIMARY KEY (`idMunicipios`),
-  INDEX `fk_Municipios_Departamentos1_idx` (`Departamentos_id` ASC),
+  INDEX `fk_Municipios_Departamentos1_idx` (`Departamentos_id` ASC) ,
   CONSTRAINT `fk_Municipios_Departamentos1`
     FOREIGN KEY (`Departamentos_id`)
     REFERENCES `mydb`.`Departamentos` (`id`)
@@ -134,3 +134,4 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
